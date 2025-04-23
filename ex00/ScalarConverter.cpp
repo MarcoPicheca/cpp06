baccc54edfeee8967b80689b4dd6dc0d047a68ef
Marco Picheca <marco.picheca@gmail.com>
2025-04-23 20:19:11 +0200

    add comment

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -21,51 +21,60 @@ void printConversions(double d)
 
 void ScalarConverter::convert(const std::string& input)
 {
-	if (input == "nanf" || input == "+inff" || input == "-inff") {
+	// gestione input uguale a nanf, nan, -inf, +inf, +inff, -inff
+	if (input == "nanf" || input == "+inff" || input == "-inff")
+	{
 		float f = std::strtof(input.c_str(), NULL);
 		std::cout << "char: impossible\nint: impossible\n";
 		std::cout << "float: " << input << "\n";
 		std::cout << "double: " << static_cast<double>(f) << "\n";
-		return;
-	} else if (input == "nan" || input == "+inf" || input == "-inf") {
+		return ;
+	}
+	else if (input == "nan" || input == "+inf" || input == "-inf")
+	{
 		double d = std::strtod(input.c_str(), NULL);
 		std::cout << "char: impossible\nint: impossible\n";
 		std::cout << "float: " << static_cast<float>(d) << "f\n";
 		std::cout << "double: " << input << "\n";
-		return;
+		return ;
 	}
-	if (input.length() == 1 && !std::isdigit(input[0])) {
+	if (input.length() == 1 && !std::isdigit(input[0]))
+	{
 		char c = input[0];
 		std::cout << "char: '" << c << "'\n";
 		std::cout << "int: " << static_cast<int>(c) << "\n";
 		std::cout << std::fixed << std::setprecision(1);
 		std::cout << "float: " << static_cast<float>(c) << "f\n";
 		std::cout << "double: " << static_cast<double>(c) << "\n";
-		return;
+		return ;
 	}
-	if (input.length() == 3 && input[0] == '\'' && input[2] == '\'') {
+	if (input.length() == 3 && input[0] == '\'' && input[2] == '\'')
+	{
 		char c = input[1];
 		std::cout << "char: '" << c << "'\n";
 		std::cout << "int: " << static_cast<int>(c) << "\n";
 		std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(c) << "f\n";
 		std::cout << "double: " << static_cast<double>(c) << "\n";
-		return;
+		return ;
 	}
-	if (!input.empty() && input[input.length() - 1] == 'f') {
+	if (!input.empty() && input[input.length() - 1] == 'f')
+	{
 		char* end;
 		float f = std::strtof(input.c_str(), &end);
-		if (*end != 'f' || *(end + 1) != '\0') {
+		if (*end != 'f' || *(end + 1) != '\0')
+		{
 			std::cout << "Invalid float input\n";
-			return;
+			return ;
 		}
 		printConversions(static_cast<double>(f));
-		return;
+		return ;
 	}
 	char* end;
 	double d = std::strtod(input.c_str(), &end);
-	if (*end != '\0') {
+	if (*end != '\0')
+	{
 		std::cout << "Invalid double/int input\n";
-		return;
+		return ;
 	}
 	printConversions(d);
 }
